ad14e0433792ad0ba26e7ea6c605aa29af88c65c
Ivan <ivan18-18@example.com>
2023-01-15 17:11:12 +0300

    третья задача готова

--- Task_29/Program.cs
+++ Task_29/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ //Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+            
+Console.WriteLine("Будет создан массив длиной A в диапвзоне 0-A");
+Console.Write("Введите любое число (A): ");
+int enterNumber = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[enterNumber];
+
+for (int i = 0; i < enterNumber; i++)
+{
+    array[i] = new Random().Next(enterNumber+1);
+    Console.Write(array[i] + "; ");
+}
+Console.WriteLine();
